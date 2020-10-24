76273e7e775de2bbe313bd6cb0e808c9761c62e9
quanlinlin <quanlinlin@xuejj.com>
2020-10-24 16:46:51 +0800

    exercise 1.4

--- part1/src/index.js
+++ part1/src/index.js
@@ -16,32 +16,46 @@ const Part = (props) => {
 const Content = (props) => {
   return (
     <div>
-      <Part partx={props.p1} />
-      <Part partx={props.p2} />
-      <Part partx={props.p3} />
+      <Part partx={props.parts[0]} />
+      <Part partx={props.parts[1]} />
+      <Part partx={props.parts[2]} />
     </div>
+  );
+};
+
+const Total = (props) => {
+  return (
+    <p>
+      Number of exercises{" "}
+      {props.parts[0].exercises +
+        props.parts[1].exercises +
+        props.parts[2].exercises}
+    </p>
   );
 };
 
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = {
-    name: "Fundamentals of React",
-    exercises: 10,
-  };
-  const part2 = {
-    name: "Using props to pass data",
-    exercises: 7,
-  };
-  const part3 = {
-    name: "State of a component",
-    exercises: 14,
-  };
+  const parts = [
+    {
+      name: "Fundamentals of React",
+      exercises: 10,
+    },
+    {
+      name: "Using props to pass data",
+      exercises: 7,
+    },
+    {
+      name: "State of a component",
+      exercises: 14,
+    },
+  ];
 
   return (
     <div>
       <Header name={course} />
-      <Content p1={part1} p2={part2} p3={part3} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   );
 };
