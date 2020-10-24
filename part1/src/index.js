923ba7692c20cbf86eae1524f028f8336f6a6c03
quanlinlin <quanlinlin@xuejj.com>
2020-10-24 18:06:16 +0800

    stateful component

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,64 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
-const Header = (props) => {
-  console.log(props);
-  return <h1>{props.name}</h1>;
-};
-
-const Part = (props) => {
-  return (
-    <p>
-      {props.partx.name} {props.partx.exercises}
-    </p>
-  );
-};
-const Content = (props) => {
-  return (
-    <div>
-      <Part partx={props.parts[0]} />
-      <Part partx={props.parts[1]} />
-      <Part partx={props.parts[2]} />
-    </div>
-  );
-};
-
-const Total = (props) => {
-  return (
-    <p>
-      Number of exercises{" "}
-      {props.parts[0].exercises +
-        props.parts[1].exercises +
-        props.parts[2].exercises}
-    </p>
-  );
-};
-
 const App = () => {
-  const course = {
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-      },
-    ],
-  };
-
-  return (
-    <div>
-      <Header name={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  );
+  const [counter, setCounter] = useState(0);
+  setTimeout(() => setCounter(counter + 1), 1000);
+  console.log("rendering...", counter);
+  return <div>{counter}</div>;
 };
+
 ReactDOM.render(<App />, document.getElementById("root"));
